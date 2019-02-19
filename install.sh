7ab71a365ff47f55576c8939c755db3563515a49
Blake Cicenas <bjcicenas@gmail.com>
2019-02-19 09:08:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo mv synchronize.sh /usr/local/sbin
-sudo sed '$isleep 30'
+sudo sed '$isleep 30' /etc/rc.local
 sudo sed '$ibash /usr/local/sbin/synchronize.sh' /etc/rc.local
 
 exit 0
