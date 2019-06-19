5443d8cf7cd2e3935331ad69fdbe5994b6ebbef6
Blake Cicenas <bjcicenas@gmail.com>
2019-06-19 09:30:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo mv synchronize.sh /usr/local/sbin
 sudo sed -i '$isleep 30' /etc/rc.local
 sudo sed -i '$ibash /usr/local/sbin/synchronize.sh' /etc/rc.local
 cd ..
-sudo rm -r Workaround-For-When-the-NTP-Port-is-Blocked
+sudo rm -r ~/Downloads/Workaround-For-When-the-NTP-Port-is-Blocked
 
 exit 0
